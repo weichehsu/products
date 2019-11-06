ac4be05b33df314b5c06b89fda0c9a09d6faaf9c
Wayne Hsu <wayne258675@hotmail.com>
2019-11-06 23:56:44 +0800

    檢查檔案在不在

--- products.py
+++ products.py
@@ -1,4 +1,20 @@
+import os #operating system
+
 products = []
+if os.path.isfile('products.csv'):
+	print('Yeah~找到檔案了')
+	#讀取檔案
+	with open('products.csv', 'r',encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+else:
+	print('找不到檔案了')
+	
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -6,13 +22,13 @@ while True:
 	price = input('請輸入價格: ')
 	price = int(price)
 	products.append([name, price])
-
-
 print(products)
 
+#印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
